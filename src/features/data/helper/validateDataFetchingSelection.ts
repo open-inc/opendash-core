ba2f43b66f39168fc8062a598816ba429ae2ac44
Sebastian Zilles <dev@zilles.io>
2020-07-23 16:18:51 +0200

    Fix validateDataFetchingSelection labels

--- src/features/data/helper/validateDataFetchingSelection.ts
+++ src/features/data/helper/validateDataFetchingSelection.ts
@@ -19,65 +19,65 @@ export function validateDataFetchingSelection(
   selection: DataFetchingOptionsInterface
 ): [boolean, string] {
   if (!config.live && selection.live) {
-    return [false, "error.data.live_forbidden"];
+    return [false, "opendash:error.data.live_forbidden"];
   }
 
   if (config.liveRequired && !selection.live) {
-    return [false, "error.data.live_required"];
+    return [false, "opendash:error.data.live_required"];
   }
 
   if (!config.history && selection.historyType) {
-    return [false, "error.data.historyType_forbidden"];
+    return [false, "opendash:error.data.historyType_forbidden"];
   }
 
   if (config.history) {
     if (config.historyRequired && !selection.historyType) {
-      return [false, "error.data.historyType_required"];
+      return [false, "opendash:error.data.historyType_required"];
     }
 
     if (selection.historyType === "absolute") {
       if (!selection.start) {
-        return [false, "error.data.start_required"];
+        return [false, "opendash:error.data.start_required"];
       }
 
       if (!selection.end) {
-        return [false, "error.data.end_required"];
+        return [false, "opendash:error.data.end_required"];
       }
     } else if (selection.historyType === "relative") {
       if (!selection.value) {
-        return [false, "error.data.value_required"];
+        return [false, "opendash:error.data.value_required"];
       }
 
       if (!selection.unit) {
-        return [false, "error.data.unit_required"];
+        return [false, "opendash:error.data.unit_required"];
       }
     } else {
-      return [false, "error.data.historyType_invalid"];
+      return [false, "opendash:error.data.historyType_invalid"];
     }
   }
 
   if (config.historyForceRelative && selection.historyType !== "relative") {
-    return [false, "error.data.historyType_invalid"];
+    return [false, "opendash:error.data.historyType_invalid"];
   }
 
   if (config.historyForceAbsolute && selection.historyType !== "absolute") {
-    return [false, "error.data.historyType_invalid"];
+    return [false, "opendash:error.data.historyType_invalid"];
   }
 
   if (!config.aggregation && selection.aggregation) {
-    return [false, "error.data.aggregation_forbidden"];
+    return [false, "opendash:error.data.aggregation_forbidden"];
   }
 
   if (config.aggregationRequired && !selection.aggregation) {
-    return [false, "error.data.aggregation_required"];
+    return [false, "opendash:error.data.aggregation_required"];
   }
 
   if (!config.resolution && selection.resolution) {
-    return [false, "error.data.resolution_forbidden"];
+    return [false, "opendash:error.data.resolution_forbidden"];
   }
 
   if (config.resolutionRequired && !selection.resolution) {
-    return [false, "error.data.resolution_required"];
+    return [false, "opendash:error.data.resolution_required"];
   }
 
   return [true, ""];
