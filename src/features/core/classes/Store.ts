3a5b4d2926ca8d95eef8f3befb5eabf7a956e5d9
Sebastian Zilles <dev@zilles.io>
2021-07-30 18:29:19 +0200

    Update Store - allow caching state

--- src/features/core/classes/Store.ts
+++ src/features/core/classes/Store.ts
@@ -4,13 +4,35 @@ type Selector<T, ReturnType> = (state: T) => ReturnType;
 type SubscriptionCallback<U> = (selection: U) => void;
 type ProducerCallback<T> = (currentState: T) => T | void;
 
-export class Store<T> {
+interface StoreCacheHandler {
+  get(key: string): Promise<any>;
+  set(key: string, value: any): Promise<void>;
+}
+
+interface StoreConfig<T> {
+  cacheHandler: StoreCacheHandler | null;
+  cacheKey: string | null;
+  cacheAllowlist: string[];
+  cacheBlocklist: string[];
+}
+
+export class Store<T = {}> {
   private _state: T;
   private watcher = new Set<SubscriptionCallback<T>>();
   private selectorCache = new Map<Selector<T, any>, any>();
+  private config: StoreConfig<T>;
 
-  constructor(initialState: T) {
+  constructor(initialState: T, config?: Partial<StoreConfig<T>>) {
     this._state = initialState;
+
+    this.config = {
+      cacheHandler: config?.cacheHandler || null,
+      cacheKey: config?.cacheKey || null,
+      cacheAllowlist: config?.cacheAllowlist || null,
+      cacheBlocklist: config?.cacheBlocklist || null,
+    };
+
+    this.readCache();
   }
 
   getState(): T {
@@ -21,15 +43,8 @@ export class Store<T> {
     return selector(this._state);
   }
 
-  update(callbackOrState: T | ProducerCallback<T>): void {
-    if (typeof callbackOrState === "function") {
-      this._state = produce(
-        this._state,
-        callbackOrState as ProducerCallback<T>
-      ) as T;
-    } else {
-      this._state = callbackOrState;
-    }
+  setState(state: T) {
+    this._state = state;
 
     this.watcher.forEach((callback) => {
       try {
@@ -38,6 +53,22 @@ export class Store<T> {
         console.error("Error in Store subscription callback:", error);
       }
     });
+
+    this.writeCache();
+  }
+
+  update(callbackOrState: T | ProducerCallback<T>): void {
+    if (typeof callbackOrState === "function") {
+      this.setState(
+        produce(this._state, callbackOrState as ProducerCallback<T>) as T
+      );
+    } else {
+      this.setState(callbackOrState);
+    }
+  }
+
+  assignToState(state: T) {
+    this.setState(Object.assign({}, this._state, state));
   }
 
   subscribe(callback: SubscriptionCallback<T>) {
@@ -69,5 +100,66 @@ export class Store<T> {
       this.watcher.delete(selectorCallback);
       this.selectorCache.delete(selector);
     };
+  }
+
+  private async readCache() {
+    if (this.config.cacheHandler && this.config.cacheKey) {
+      try {
+        const handler = this.config.cacheHandler;
+        const key = `store:cache:${this.config.cacheKey}`;
+
+        let cache = await handler.get(key);
+
+        cache = this.handleCacheLists(cache);
+
+        this.assignToState(cache);
+      } catch (error) {
+        console.error(`@opendash/store: Error while loading cache:`, error);
+      }
+    }
+  }
+  private async writeCache() {
+    if (this.config.cacheHandler && this.config.cacheKey) {
+      try {
+        const handler = this.config.cacheHandler;
+        const key = `store:cache:${this.config.cacheKey}`;
+
+        const cache = this.handleCacheLists(this._state);
+
+        await handler.set(key, cache);
+
+        console.log(`@opendash/store: Cached '${key}':`, cache);
+      } catch (error) {
+        console.error(`@opendash/store: Error while writing cache:`, error);
+      }
+    }
+  }
+
+  private handleCacheLists(state: T) {
+    const result = { ...state };
+
+    if (
+      Array.isArray(this.config.cacheAllowlist) &&
+      this.config.cacheAllowlist.length > 0
+    ) {
+      for (const key of Object.keys(result)) {
+        if (!this.config.cacheAllowlist.includes(key)) {
+          delete result[key];
+        }
+      }
+    }
+
+    if (
+      Array.isArray(this.config.cacheBlocklist) &&
+      this.config.cacheBlocklist.length > 0
+    ) {
+      for (const key of Object.keys(result)) {
+        if (this.config.cacheBlocklist.includes(key)) {
+          delete result[key];
+        }
+      }
+    }
+
+    return result;
   }
 }
